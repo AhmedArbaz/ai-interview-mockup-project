9af7df0d7c2919097cd677e4be1816df4f22fc21
AhmedArbaz <ah2797764@gmail.com>
2025-09-22 19:27:20 +0500

    Add a shadCN button to the application

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div className="bg-amber-400">
     Hello
+    <Button>Subscribe</Button>
    </div>
   );
 }
---
+++ personalNotes.md
@@ -0,0 +1,8 @@
+## Shad cn installation
+Vo to install kar lia ab ak button banana tha to direct 
+```
+npx shadcn@latest add button
+```
+ya command chala di button.jsx in components/ui/button.jsx khud ban gai
+
+
